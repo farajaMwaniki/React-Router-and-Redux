1ef26c9b9a741bea9420e49a533dbdca283fdf25
Walter Mwaniki <54999304+farajaMwaniki@users.noreply.github.com>
2022-04-20 23:07:20 +0000

    Styling complete for videos page

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,23 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import Home from './components/home.jsx';
-import Utube from './components/utube.jsx'
+import Utube from './components/utube.jsx';
+import About from './components/about.jsx'
 
 const Nav = (props) => {
   return (
-    <nav>
-      <NavLink to="/" className="logo">UTube</NavLink>
-      <div className="navlinks">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/videos">Videos</NavLink>
-        <NavLink to="/about">About</NavLink>
-      </div>
-    </nav>
+		<div className="nav-bar">
+	    <nav>
+					<NavLink to="/" className="logo">UTube</NavLink>
+	      	<div className="navlinks">
+		        <NavLink to="/">Home</NavLink>
+		        <NavLink to="/videos">Videos</NavLink>
+		        <NavLink to="/about">About</NavLink>
+	      	</div>
+	    </nav>
+		</div>
   );
 };
-
-const About = (props) => {
-  return (
-    <div>
-      <h1> About Us</h1>
-      <h2> Vassar IEEE</h2>
-      <p> Email: </p> 
-      <p> gbelaineh@vassar.edu </p> 
-      <p> wmwaniki@vassar.edu </p>
-    </div>
-  )
-}
 
 function App() {
   return (
---
+++ src/components/about.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useCallback } from 'react';
+
+const About = (props) => {
+  return (
+    <div className="about-body">
+      <h1> About Us</h1>
+      <h2> Vassar IEEE</h2>
+      <p> Email: </p> 
+      <p> gbelaineh@vassar.edu </p> 
+      <p> wmwaniki@vassar.edu </p>
+    </div>
+  )};
+
+export default About;
